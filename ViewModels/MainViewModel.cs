102c1e920a1e1ca4d60fccfe541915ff5c2d66ad
NikSan Job PC <Nik.San.3452@gmail.com>
2024-07-02 16:35:46 +0500

    Добавлена MainViewModel

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Markup;
+using EmailAttachmentExtractor.ViewModels.Base;
+
+namespace EmailAttachmentExtractor.ViewModels;
+
+[MarkupExtensionReturnType(typeof(MainViewModel))]
+public class MainViewModel: ViewModel
+{
+    
+}

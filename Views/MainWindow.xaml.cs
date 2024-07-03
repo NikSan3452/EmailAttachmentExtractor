1312c3c300e6aed3e44de313f540968dec66876b
NikSan3452 <nik.san.3452@gmail.com>
2024-07-03 19:42:34 +0500

    Добавлен DataContext

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,14 +1,13 @@
 ﻿using System.Windows;
+using EmailAttachmentExtractor.ViewModels;
 
 namespace EmailAttachmentExtractor.Views;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainViewModel();
     }
 }
